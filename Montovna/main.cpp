0ff59b8cf34db8c26f38dbb4c122f976ec18d852
Baerdon <jardabazant@seznam.cz>
2017-12-11 14:16:27 +0100

    Initial Commit

---
+++ Montovna/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Montovna
+//
+//  Created by Jaroslav Bažant on 11.12.17.
+//  Copyright © 2017 Jaroslav Bažant. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
