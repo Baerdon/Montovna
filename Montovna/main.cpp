1bb6f7c5e000ac8df23c40fe6c27cd95f9ea8f73
Baerdon <jardabazant@seznam.cz>
2017-12-11 15:01:43 +0100

    Zásobník
    přidána třída zásobníku

--- Montovna/main.cpp
+++ Montovna/main.cpp
@@ -7,6 +7,44 @@
 //
 
 #include <iostream>
+using namespace std;
+
+class Uzel {
+    int cas_vyroby;
+    Uzel *sousede[];
+};
+
+class Zasobnik {
+private:
+    int vrchol;
+    int velikost;
+    Uzel *hodnoty;
+public:
+    Zasobnik (int velikost){
+        this->velikost = velikost;
+        hodnoty = new Uzel[velikost];
+        vrchol = -1;
+    }
+    
+    ~Zasobnik(){
+        delete[] hodnoty;
+    }
+    
+    void Push(Uzel u){ //tady možná testovat plnost
+        vrchol++;
+        hodnoty[vrchol] = u;
+    }
+    
+    Uzel pop(){
+        Uzel u = hodnoty[vrchol];
+        vrchol--;
+        return u;
+    }
+    
+    bool isEmpty(){
+        return vrchol == -1;
+    }
+};
 
 int main(int argc, const char * argv[]) {
     // insert code here...
